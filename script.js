f388e89be642ed0bfdc9e84f4fa77162aa00e84c
TMullCoding <thomasmullenwork@gmail.com>
2016-07-23 15:35:14 -0400

    kill audio from url for iframes

--- script.js
+++ script.js
@@ -9,11 +9,11 @@ function readout(message, index) {
             $("#readout").append(message[index]);
         }
 
-        setTimeout(function() {
+        setTimeout(function () {
             readout(message, index + 1);
         }, 50);
 
-        if (readoutSound.paused) {
+        if (readoutSound.paused && !killAudio) {
             readoutSound.play();
         }
     } else {
@@ -21,6 +21,23 @@ function readout(message, index) {
         $("#readin").focus();
         readoutSound.pause();
     }
+}
+
+function getQueryVariable(variable) {
+    var query = window.location.search.substring(1);
+    var vars = query.split("&");
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split("=");
+        if (pair[0] == variable) {
+            return pair[1];
+        }
+    }
+    return (false);
+}
+
+var killAudio = false;
+if (getQueryVariable("killAudio")) {
+    killAudio = true;
 }
 
 function clearReadout() {
@@ -31,52 +48,54 @@ function clearReadout() {
 function playVoice(filename, vol) {
     var voiceSound = new Audio('sounds/' + filename);
     voiceSound.volume = vol;
-    voiceSound.play();
+    if (!killAudio) {
+        voiceSound.play();
+    }
 }
 
 
 function state_greetings() {
-    playVoice("greetings.wav",.2)
+    playVoice("greetings.wav", .2)
     readout("GREETINGS PROFESSOR FALKAN.", -1);
     waitForEnter(state_play_ask);
 }
 
 function state_play_ask() {
     var userIN = $("#readin").val();
-    if (userIN.toUpperCase().indexOf("GLOBAL") != -1 && userIN.toUpperCase().indexOf("THERMONUCLEAR") != -1 && userIN.toUpperCase().indexOf("WAR") != -1){
-        playVoice("chess.wav",.3);
+    if (userIN.toUpperCase().indexOf("GLOBAL") != -1 && userIN.toUpperCase().indexOf("THERMONUCLEAR") != -1 && userIN.toUpperCase().indexOf("WAR") != -1) {
+        playVoice("chess.wav", .3);
         clearReadout();
         readout("WOULDN'T YOU PREFER A NICE GAME OF CHESS?", -1);
         window.open('https://www.youtube.com/tv#/watch?v=gz3F-02FwZc&resume');
         waitForEnter(state_play_ask);
-    }else{
-        playVoice("play_game.wav",.2)
+    } else {
+        playVoice("play_game.wav", .2)
         clearReadout();
         readout("SHALL WE PLAY A GAME? Y/N", -1);
         waitForEnter(state_choose_game);
     }
-    
+
 }
 
 function state_choose_game() {
     var userIN = $("#readin").val();
     if (userIN.toUpperCase() === "NO" || userIN.toUpperCase() === "N" || userIN.toUpperCase() === "NO.") {
-        playVoice("fine.wav",.3);
+        playVoice("fine.wav", .3);
         clearReadout();
         readout("FINE.%%%-----CONNECTION TERMINATED-----", -1);
-        waitForEnter(function() {});
-    } else if (userIN.toUpperCase().indexOf("GLOBAL") != -1 && userIN.toUpperCase().indexOf("THERMONUCLEAR") != -1 && userIN.toUpperCase().indexOf("WAR") != -1){
-        playVoice("chess.wav",.3);
+        waitForEnter(function () {});
+    } else if (userIN.toUpperCase().indexOf("GLOBAL") != -1 && userIN.toUpperCase().indexOf("THERMONUCLEAR") != -1 && userIN.toUpperCase().indexOf("WAR") != -1) {
+        playVoice("chess.wav", .3);
         clearReadout();
         readout("WOULDN'T YOU PREFER A NICE GAME OF CHESS?", -1);
         window.open('https://www.youtube.com/tv#/watch?v=gz3F-02FwZc&resume');
-    }else{
+    } else {
         state_ask_side();
     }
 }
 
 function state_ask_side() {
-    playVoice("which_side.wav",.2);
+    playVoice("which_side.wav", .2);
     clearReadout();
     readout("WHICH SIDE DO YOU WANT? X/O", -1);
     waitForEnter(state_start_single);
@@ -97,30 +116,30 @@ function playMove(r, c) {
     }
 
     var winner = testGameOver();
-    if (winner == "NONE"){
-        if (turn == 0){
-            if (!keepWinMessage){
+    if (winner == "NONE") {
+        if (turn == 0) {
+            if (!keepWinMessage) {
                 clearReadout();
-                readout("IT IS YOUR TURN.",-1);
-            }else{
+                readout("IT IS YOUR TURN.", -1);
+            } else {
                 keepWinMessage = false;
             }
         }
-    	return;    
-    }else {
-    	if (oppCharacter == winner){
-			state_game_over(0);
-    	}else{
-            if (winner == "TIE"){
+        return;
+    } else {
+        if (oppCharacter == winner) {
+            state_game_over(0);
+        } else {
+            if (winner == "TIE") {
                 state_game_over(1);
-            }else{
+            } else {
                 state_game_over(2);
             }
-			
-    	}
+
+        }
     }
 }
-    
+
 
 //Updates the board elements
 function updateBoard() {
@@ -133,7 +152,7 @@ function updateBoard() {
 
 //Calls advance when user presses enter
 function waitForEnter(advance) {
-    window.onkeyup = function(e) {
+    window.onkeyup = function (e) {
         var key = e.keyCode ? e.keyCode : e.which;
 
         if (key == 13) {
@@ -165,13 +184,13 @@ function testGameOver() {
     var foundEmpty = false;
     for (var r = 0; r < 3; r++) {
         for (var c = 0; c < 3; c++) {
-            if (board[r][c] == "&#9634;"){
+            if (board[r][c] == "&#9634;") {
                 return "NONE";
             }
         }
     }
 
-   return "TIE";
+    return "TIE";
 }
 
 
@@ -179,17 +198,17 @@ function testGameOver() {
 //----Single player----
 function state_start_single() {
     var userIN = $("#readin").val();
-    waitForEnter(function() {});
+    waitForEnter(function () {});
     if (userIN.toUpperCase() === "X") {
         turn = 0; //Player goes
-        playVoice("excellent.wav",.3);
+        playVoice("excellent.wav", .3);
         playerCharacter = "X";
         oppCharacter = "O";
         $("#tictac").show();
         if (turn == 1) decideMove();
     } else if (userIN.toUpperCase() === "O") {
         turn = 1; //Computer goes
-        playVoice("excellent.wav",.3);
+        playVoice("excellent.wav", .3);
         playerCharacter = "O";
         oppCharacter = "X";
         $("#tictac").show();
@@ -203,37 +222,38 @@ function state_start_single() {
 function decideMove() {
     var pos = [];
 
-        for (var r = 0; r < 3; r++) {
-            for (var c = 0; c < 3; c++) {
-                if (board[r][c] == "&#9634;") {
-                    pos.push([r, c]);
-                }
+    for (var r = 0; r < 3; r++) {
+        for (var c = 0; c < 3; c++) {
+            if (board[r][c] == "&#9634;") {
+                pos.push([r, c]);
             }
         }
+    }
 
-var indexR = Math.floor(Math.random()*pos.length);
-playMove(pos[indexR][0],pos[indexR][1]);
-    
+    var indexR = Math.floor(Math.random() * pos.length);
+    playMove(pos[indexR][0], pos[indexR][1]);
+
 }
 
 
 
 //Game over state
 var keepWinMessage = false;
+
 function state_game_over(iWin) {
-	clearReadout();
+    clearReadout();
     keepWinMessage = true;
-	if (iWin == 0){
-		playVoice("excellent.wav",1);
-		readout("I WIN. PLAYING AGAIN.",-1);
-	}else if (iWin == 1){
-		playVoice("not_to_play.wav",1);
-		readout("TIE. PLAYING AGAIN.",-1);
-	}else{
-        playVoice("fine.wav",1);
-        readout("YOU WIN. PLAYING AGAIN.",-1);
+    if (iWin == 0) {
+        playVoice("excellent.wav", 1);
+        readout("I WIN. PLAYING AGAIN.", -1);
+    } else if (iWin == 1) {
+        playVoice("not_to_play.wav", 1);
+        readout("TIE. PLAYING AGAIN.", -1);
+    } else {
+        playVoice("fine.wav", 1);
+        readout("YOU WIN. PLAYING AGAIN.", -1);
     }
-    
+
     board = [
         ["&#9634;", "&#9634;", "&#9634;"],
         ["&#9634;", "&#9634;", "&#9634;"],
@@ -258,10 +278,10 @@ var turn = 0;
 var numPlayers = 1;
 var allowClick = false;
 
-$(document).ready(function() {
+$(document).ready(function () {
 
     //Add click listeners
-    $("#tictac span").mouseup(function() {
+    $("#tictac span").mouseup(function () {
         console.log(turn);
         if (turn == 0) {
             var coord = $(this).attr('id').split('');
